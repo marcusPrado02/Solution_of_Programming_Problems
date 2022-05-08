4559e5c0a1c7bfcd6fd852c318ce74411ea5d05e
marcusPrado02 <silvamarcusprado@gmail.com>
2022-05-08 17:49:47 -0300

    TwoSum question Cpp solution

---
+++ Easy/TwoSum/TwoSum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+
+class TwoSum{
+public:
+     vector<int> twoSum(vector<int>& nums, int target){
+          vector<int> result;
+
+          for(auto it1 = nums.begin(); it1 != nums.end(); ++it1){
+                  auto it2 = find(it1 + 1,nums.end(),target - *it1);
+                  if(it2 != nums.end()){
+                       result.push_back(it1 - nums.begin());
+                       result.push_back(it2 - nums.begin());
+                       break;
+                  }
+
+          }
+          return result;
+
+    }
+
+};
