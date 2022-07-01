6872bd2f9b33746ef8a51b4d58e5230fad67ada5
marcusPrado02 <silvamarcusprado@gmail.com>
2022-07-01 16:22:12 -0300

    Solution Implement Strstr problem in C++

---
+++ Easy/ImplementStrstr/ImplementStrstr.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int strStr(string haystack, string needle) {
+            for (int i = 0; i + needle.length() < haystack.length() + 1; ++i) {
+                        if (haystack.substr(i, needle.length()) == needle) {
+                                        return i;
+                                    }
+                    }
+            return -1;
+        }
+};
